95cb6041a7d9a03d171fb662dd7f8a373c869b7f
hengchomroeunnn <hengchomroeun3103@gmail.com>
2024-09-13 23:46:50 +0700

    inisght

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
+    <SpeedInsights />
     <Navbar />
     <slot />
     <Footer />
   </div>
 </template>
+
+<script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
