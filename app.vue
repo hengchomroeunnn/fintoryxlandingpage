8594ba2e6a4e0edd4ded3c4118f19b884842dcd5
hengchomroeunnn <hengchomroeun3103@gmail.com>
2024-10-15 17:14:36 +0700

    sme

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@ import { useI18n } from "vue-i18n";
 const { locale } = useI18n();
 
 useHead({
-  title: "Fintory - Mobile POS System for Convenience Stores & SME   s",
+  title: "Fintory - Mobile POS System for Convenience Stores & SMEs",
   meta: [
     { name: "description", content: "Fintory is a cloud-based mobile POS system designed for convenience stores and SMEs in Cambodia. Manage sales, inventory, invoicing, payments, and more, all in one app." },
     { name: "keywords", content: "Fintory, mPOS, mobile POS, convenience store, SMEs, Cambodia, inventory management, sales processing, invoicing, payment integration, FlutterFlow" },
