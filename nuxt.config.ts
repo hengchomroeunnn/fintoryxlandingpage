c670b0931f77ca19f269ee142e88a60f23ddde03
hengchomroeunnn <hengchomroeun3103@gmail.com>
2024-10-15 17:20:51 +0700

    meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,32 @@ import { config } from "dotenv";
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
+      title: "Fintory - Mobile POS System",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Fintory is a cloud-based mobile POS system...",
+        },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }, // Favicon in public directory
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+      ],
+    },
   },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
@@ -29,14 +52,13 @@ export default defineNuxtConfig({
       sessionCookie: false,
     },
     config: {
-      apiKey: 'AIzaSyBD_pILvWIrotuQZwe20joZDoYMWCWP3yg',
-      authDomain: 'fintoryemail.firebaseapp.com',
-      projectId: 'fintoryemail',
-      storageBucket: 'fintoryemail.appspot.com',
-      messagingSenderId: '374688381548',
-      appId: '1:374688381548:web:77d7821b0da7cada583649',
-      measurementId: 'G-YCSNM86PFK' 
+      apiKey: "AIzaSyBD_pILvWIrotuQZwe20joZDoYMWCWP3yg",
+      authDomain: "fintoryemail.firebaseapp.com",
+      projectId: "fintoryemail",
+      storageBucket: "fintoryemail.appspot.com",
+      messagingSenderId: "374688381548",
+      appId: "1:374688381548:web:77d7821b0da7cada583649",
+      measurementId: "G-YCSNM86PFK",
     },
   },
-  
 });
