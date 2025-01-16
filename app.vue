fa15ed2ecbc6a39f19e6ec7e0cfc97a5f5264be7
HENG CHOMROEUN <127354387+hengchomroeunnn@users.noreply.github.com>
2025-01-16 11:35:28 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -12,4 +12,15 @@ import { useI18n } from "vue-i18n";
 
 const { locale } = useI18n();
 
+export default {
+  mounted() {
+    const script = document.createElement("script");
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8108895822236328";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+  },
+};
+
+
 </script>
